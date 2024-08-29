056552036ddd53bbae62220758bf611ce5c50a81
Rana718 <ranadolui718@gmail.com>
2024-08-30 00:56:46 +0530

    fix the next song overlap issue

--- backend/Playlric/song_info/data/tasks.py
+++ backend/Playlric/song_info/data/tasks.py
@@ -1,7 +1,12 @@
 from .utils import get_video_data
+from .music_info import all_details
+
 def fetch_next_video_data(video_url):
     try:
-        next_video_data = get_video_data(video_url)
-        print(f"Next video data: {next_video_data}")
+        next_video_urls = get_video_data(video_url)
+        next_video_all_data = all_details(next_video_urls, is_url=True)
+        return next_video_all_data
+        
     except Exception as e:
         print(f"Error fetching next video data: {e}")
+        return {'error': str(e)}
