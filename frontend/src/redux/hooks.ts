eb6c604649cdc9ac56c2edaa24ceb7368b4e4841
Rana718 <ranadolui718@gmail.com>
2024-09-21 13:48:01 +0530

    Remove frontend folder from Git tracking

--- frontend/src/redux/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { AppDispatch, RootState } from './store';
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: <T>(selector: (state: RootState) => T) => T = useSelector;
